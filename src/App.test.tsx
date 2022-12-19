d50477c6f474d1b44b9a41b2f09e23fab35d356f
Artur Vieira <artur.vieira@castoredc.com>
2022-12-19 18:57:35 +0100

    Trying to test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, act } from '@testing-library/react';
 import App from './App';
 
 jest.mock('chart.js', () => ({ Chart: { register: jest.fn } }));
 jest.mock('react-chartjs-2', () => ({}));
+
+jest.mock('./providers/exchange-rates', () => ({
+	getCurrencies: () =>
+		Promise.resolve([
+			{ symbol: 'EUR', name: 'EUR' },
+			{ symbol: 'BRL', name: 'BRL' },
+		]),
+	getTimeSeries: () =>
+		Promise.resolve({
+			success: true,
+			timeseries: true,
+			start_date: '2012-05-01',
+			end_date: '2012-05-03',
+			base: 'EUR',
+			targe: 'BRL',
+			rates: {
+				'2012-05-01': {
+					BRL: 1.302303,
+				},
+				'2012-05-02': {
+					BRL: 1.315066,
+				},
+				'2012-05-03': {
+					BRL: 1.280135,
+				},
+			},
+		}),
+	convertCurrency: () =>
+		Promise.resolve({
+			date: '2022-12-17',
+			info: { rate: 0.872809, timestamp: 1671297122 },
+			query: { amount: 1, from: 'EUR', to: 'BRL' },
+			result: 0.872809,
+			success: true,
+		}),
+}));
 
 test('render base component', () => {
 	render(<App />);
@@ -17,4 +53,25 @@ test('render base component', () => {
 	expect(targetCurrency).toBeInTheDocument();
 	expect(additionalInformation).not.toBeInTheDocument();
 	expect(chart).not.toBeInTheDocument();
+});
+
+test('render additional information and chart', async () => {
+	render(<App />);
+
+	const [baseCurrency, targetCurrency] = screen.getAllByRole('combobox');
+
+	fireEvent.keyDown(baseCurrency, { key: 'ArrowDown' });
+	fireEvent.click(await screen.findByText('EUR - EUR'));
+
+	fireEvent.keyDown(targetCurrency, { key: 'ArrowDown' });
+	fireEvent.click(await screen.findByText('BRL - BRL'));
+
+	const amountInput = screen.getByRole('spinbutton');
+	fireEvent.change(amountInput, { target: { value: '20' } });
+
+	const additionalInformation = await screen.findByRole('contentinfo');
+	expect(additionalInformation).toBeInTheDocument();
+
+	const chart = screen.queryByRole('figure');
+	expect(chart).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -78,6 +78,7 @@ export default function App() {
 	useEffect(() => {
 		convertValue(formData);
 		getRates(formData);
+		console.log(formData);
 	}, [formData, convertValue, getRates]);
 
 	return (
@@ -130,7 +131,7 @@ export default function App() {
 				</Column>
 			)}
 
-			{timeSeries && <RateChart timeSeries={timeSeries} />}
+			{/* {timeSeries && <RateChart timeSeries={timeSeries} />} */}
 		</Card>
 	);
 }
